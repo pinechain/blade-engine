92d766e830aa03dea8d130b04cf3a6f906f5fd81
PineChain <mrpinechain@gmail.com>
2021-05-14 14:28:50 -0300

    feat #1 - Environment setup genesis.

---
+++ scripts/env-setup/config/cfg_checkouts.sh
@@ -0,0 +1,7 @@
+# Checkout targets
+REPO_CHECKOUTS=( \ 
+    [BL_GAMEDEV]=""  \
+    [BL_ANDROID]=""  \ 
+    [GD_PACMAN]=""  \
+    [GD_VUESRPG]=""  \
+    [AD_SCRAPPIE]="")
---
+++ scripts/env-setup/config/cfg_defs.sh
@@ -0,0 +1,3 @@
+# Environment definitions
+ENVIRONMENT_NAME="forge"
+ENVIRONMENT_ROOT="/c/alexandria/${ENVIRONMENT_NAME}"
